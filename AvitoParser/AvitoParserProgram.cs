40bbc9585ba9c7e05118027d5cb7c560029d491c
SHILY <45015327+SHILY-PROJECT@users.noreply.github.com>
2021-09-22 20:28:31 +0300

    Creating an avito parser

---
+++ AvitoParser/AvitoParserProgram.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using HtmlAgilityPack;
+using AvitoParser.Models;
+
+namespace AvitoParser
+{
+    public class AvitoParserProgram
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Для начала парсинга нажмите Enter...");
+
+            while (true)
+            {
+                if (Console.ReadKey(true).Key == ConsoleKey.Enter)
+                {
+                    PrintColorText("Старт парсинга. Пожалуйста, подождите...", ConsoleColor.Green);
+                    break;
+                }
+            }
+
+            var htmlWeb = new HtmlWeb();
+            var products = new List<AvitoItemModel>();
+
+            var url = @"https://www.avito.ru/nizhniy_novgorod?p=2&q=rtx+3080";
+
+            htmlWeb
+                .Load(url)
+                .DocumentNode.SelectNodes("//div[@data-marker='item']").ToList()
+                .ForEach(x => products.Add(new AvitoItemModel
+                {
+                    Product = x.SelectNodes(".//h3[contains(@itemprop, 'name')]")[0].GetDirectInnerText(),
+                    Price = x.SelectNodes(".//span[contains(@class, 'price-text')]")[0].GetDirectInnerText()
+                }));
+
+            if (products.Count != 0)
+            {
+                var maxProductNameLength = products.Select(x => x.Product.Length).Max();
+                PrintColorText("Результат:", ConsoleColor.Blue);
+                products.ForEach(x => Console.WriteLine($"Товар: {x.Product}{Indent(maxProductNameLength, x)}  |  Цена: {x.Price}"));
+                
+            }
+            else PrintColorText("Простите, но по вашему запросу ничего не найдено...:(", ConsoleColor.Yellow);
+
+            Console.ReadKey();
+        }
+
+        private static void PrintColorText(string text, ConsoleColor consoleColor)
+        {
+            Console.ForegroundColor = consoleColor;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+
+        private static string Indent(int maxProductNameLength, AvitoItemModel avitoItem)
+            => new(' ', maxProductNameLength - avitoItem.Product.Length);
+    }
+}
---
+++ AvitoParser/Models/AvitoItemModel.cs
@@ -0,0 +1,10 @@
+﻿using HtmlAgilityPack;
+
+namespace AvitoParser.Models
+{
+    public class AvitoItemModel
+    {
+        public string Product { get; set; }
+        public string Price { get; set; }
+    }
+}

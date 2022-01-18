22eca7fe871ad927e6b794c4548bdf4dbdbc86fd
Ilya Shimbuev <iliyarock@gmail.com>
2022-01-18 23:05:00 +0300

    -Сравнение модели и эксепшена

---
+++ ExceptionTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using ExceptionTest.Models;
+using System;
+using System.Diagnostics;
+
+namespace ExceptionTest
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var timerModel = new Stopwatch();
+            var timerException = new Stopwatch();
+
+            timerModel.Start();
+            for (int i = 0; i < 1000; i++)
+            {
+                var content = new Content()
+                {
+                    IsSuccess = false,
+                    Message = "Error"
+                };
+
+                if (!content.IsSuccess) { }
+            }
+            timerModel.Stop();
+
+            Console.WriteLine($"Model: {timerModel.ElapsedMilliseconds} ms");
+
+            timerException.Start();
+            for (int i = 0; i < 1000; i++)
+            {
+                try
+                {
+                    throw new Exception("Error");
+                }
+                catch (Exception ex) { }
+            }
+            timerException.Stop();
+
+            Console.WriteLine($"Exception: {timerException.ElapsedMilliseconds} ms");
+
+            Console.ReadKey();
+        }
+    }
+}

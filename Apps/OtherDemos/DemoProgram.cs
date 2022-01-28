90648f9151e3affa30386ef0efef712c2d387a02
Ilya Shimbuev <iliyarock@gmail.com>
2022-01-28 21:07:46 +0300

    -Добавлена демка получения размера объекта из байтов

---
+++ Apps/OtherDemos/DemoProgram.cs
@@ -0,0 +1,16 @@
+﻿DemoExtractSizeFromBytes();
+
+Console.ReadKey();
+
+static void DemoExtractSizeFromBytes()
+{
+    var dataSet = new[] { 952, 1024, 1551, 5562, 55646546, 56466621616, 876666216163, 2746666216163 };
+
+    Printer.PrintTitle("VARIABLE HANDLER ║ ExtractSizeFromBytes");
+
+    Array.ForEach(dataSet, value =>
+    {
+        var res = VariableHandler.ExtractSizeFromBytes(value);
+        Console.WriteLine(res);
+    });
+}
---
+++ Apps/OtherDemos/Toolkit/VariableHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace OtherDemos.Toolkit
+{
+    public class VariableHandler
+    {
+        public static string ExtractSizeFromBytes(long size)
+        {
+            var endOf = new[] { "Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+            var index = default(int);
+
+            while (index <= 10)
+            {
+                if (size <= (long)Math.Pow(2, (++index * 10)))
+                {
+                    var convertedSize = (size / Math.Pow(2, (index - 1) * 10));
+                    var result = Convert.ToString(
+                        Math.Round(convertedSize, Math.Truncate(convertedSize).ToString().Length switch { 1 => 2, 2 => 1, _ => 0 }));
+                    return $"{result} {endOf[index - 1]}";
+                }
+            }
+            return "HZ";
+        }
+
+    }
+}

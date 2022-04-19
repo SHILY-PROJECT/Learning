04e1346138ba199a0987d7b9d0c8d763887b85c2
Ilya Shimbuev <iliyarock@gmail.com>
2022-04-19 18:09:03 +0300

    -Реализован класс с методами для определения данных ip

---
+++ src/IdentifyUserDataByIp/Core/Detector.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Text.Json;
+using IdentifyUserDataByIp.Core.Models;
+
+namespace IdentifyUserDataByIp.Core;
+
+internal class Detector
+{
+    public static bool TryDefineIpData(out IpData userData)
+        => (userData = DefineIpDataAsync("").Result).IsSuccess;
+
+    public static bool TryDefineIpData(string ip, out IpData userData)
+        => (userData = DefineIpDataAsync(ip).Result).IsSuccess;
+
+    public static async Task<IpData> DefineIpDataAsync()
+        => await DefineIpDataAsync("");
+
+    public static async Task<IpData> DefineIpDataAsync(string ip)
+    {
+        try
+        {
+            if (!string.IsNullOrEmpty(ip) && !IPAddress.TryParse(ip, out _))
+                throw new ArgumentException("Invalid ip address.");
+
+            using var httpClient = new HttpClient();
+            var response = await (await httpClient.GetAsync($"http://ip-api.com/json/{ip}")).Content.ReadAsStringAsync();
+
+            if (JsonSerializer.Deserialize<IpData>(response) is IpData res && res.Status.Contains("success", StringComparison.OrdinalIgnoreCase))
+            {
+                return res with { IsSuccess = true };
+            }
+            else throw new InvalidOperationException("Failed to receive data from the service.");
+        }
+        catch (Exception ex)
+        {
+            return new IpData { ErrorMessage = ex.Message, IsSuccess = false };
+        }
+    }
+}
